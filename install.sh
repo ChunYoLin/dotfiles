613f0970f257a0e14c8acb6f9ac645d11c910eb4
ChunYoLin <r04922108@csie.ntu.edu.tw>
2016-12-14 15:32:24 +0800

    .

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
-ln -s ./.vimrc $HOME/.vimrc
+mv $HOME/.vimrc $HOME/.vimrc.back
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-ln -s ./.inputrc $HOME/.inputrc
-ln -s ./.tmux.conf $HOME/.tmux.conf
-ln -s ./.bashrc $HOME/.bashrc
+mv $HOME/.inputrc $HOME/.inputrc.back
+ln -s $HOME/dotfiles/.inputrc $HOME/.inputrc
+mv $HOME/.tmux.conf $HOME/.tmux.conf.back
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+mv $HOME/.bashrc $HOME/.bashrc.back
+ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
 git clone --depth=1 https://github.com/Bash-it/bash-it.git $HOME/.bash_it
 sh $HOME/.bash_it/install.sh
